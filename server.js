127660d22b51970a4ad6c72482869be9613b0ad0
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-08-10 22:49:54 +0530

    cors-issue-fix-6

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://tomper-chat-server.herokuapp.com",
+    origin: "https://tomperchat.netlify.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
