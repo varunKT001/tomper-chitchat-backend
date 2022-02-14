0ab7a1cba6b78d55e20d8b77cf7506122ace1e3b
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-02-14 14:02:12 +0530

    endpoints changed

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const PORT = process.env.PORT || 5000;
 
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const moment = require("moment");
-const socketio = require("socket.io");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const moment = require('moment');
+const socketio = require('socket.io');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -13,68 +13,71 @@ app.use(express.urlencoded({ extended: false }));
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://tomperchat.netlify.app",
-    methods: ["GET", "POST"],
+    origin: 'https://tomper-chitchat.netlify.app',
+    methods: ['GET', 'POST'],
   },
 });
 
-const BOT = "TOMPER BOT ✔";
+const BOT = 'TOMPER BOT ✔';
 
 function formatMessage(username, text, image) {
   return {
     username,
     text,
     image,
-    time: moment().utcOffset("+05:30").format("h:mm a"),
+    time: moment().utcOffset('+05:30').format('h:mm a'),
   };
 }
 
 let users = [];
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   //ON NEW USER JOIN
-  socket.on("new-user", ({ username, room }) => {
+  socket.on('new-user', ({ username, room }) => {
     users.push({ id: socket.id, name: username, room: room });
     socket.join(room);
-    socket.emit("chat-message", formatMessage(BOT, "welcome to TOMPER CHAT!"));
+    socket.emit(
+      'chat-message',
+      formatMessage(BOT, 'welcome to TOMPER CHIT CHAT!')
+    );
     socket.broadcast
       .to(room)
-      .emit("chat-message", formatMessage(BOT, `${username} joined the room`));
+      .emit('chat-message', formatMessage(BOT, `${username} joined the room`));
     const connectedUsers = users.filter((user) => user.room === room);
-    io.to(room).emit("connected-users", connectedUsers);
+    io.to(room).emit('connected-users', connectedUsers);
   });
 
   //ON NEW CHAT MESSAGE
-  socket.on("chat-message", (message) => {
+  socket.on('chat-message', (message) => {
     let user = users.find((user) => user.id === socket.id);
     if (user) {
       io.to(user.room).emit(
-        "chat-message",
+        'chat-message',
         formatMessage(user.name, message.messageText, message.baseImage)
       );
     }
   });
 
   //ON USER TYPING
-  socket.on("typing", () => {
+  socket.on('typing', () => {
     let user = users.find((user) => user.id === socket.id);
     if (user) {
       socket.broadcast
         .to(user.room)
-        .emit("typing", `${user.name} is typing...`);
+        .emit('typing', `${user.name} is typing...`);
     }
   });
 
   //ON USER-DISCONNECT
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     let user = users.find((user) => user.id === socket.id);
     if (user) {
       socket.broadcast
         .to(user.room)
-        .emit("chat-message", formatMessage(BOT, `${user.name} left the room`));
+        .emit('chat-message', formatMessage(BOT, `${user.name} left the room`));
       users = users.filter((user) => user.id != socket.id);
       const connectedUsers = users.filter((_user) => _user.room === user.room);
-      io.to(user.room).emit("connected-users", connectedUsers);
+      io.to(user.room).emit('connected-users', connectedUsers);
     }
   });
 });
